cd39158250ed11c205e999bd7f536100012aad7d
agdfydy <hakka.ydy.sub@gmail.com>
2024-01-29 12:17:52 +0900

    t

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,13 @@ import { createVertex } from "./modules/graph/vertex.ts";
 
 const PATH = "./result.csv";
 
+const MAX_BIT_LENGTH = 200;
+const MAX_COST_VALUE = (0b1n << BigInt(MAX_BIT_LENGTH)) - 1n;
+
 function main() {
     Deno.writeTextFile(
         PATH,
-        "dimension, vertices, ave_degree, maxBitLength, epsilon, mosp, fptas_mosp, diff_cost\n",
+        "dimension, vertices, ave_degree, epsilon, mosp, fptas_mosp, cost_diff_%\n",
     );
 
     for (let dimension = 8; dimension >= 2; dimension -= 2) {
@@ -21,69 +24,65 @@ function main() {
             vertices >= 200;
             vertices -= 200
         ) {
-            for (let aveDegree = 5; aveDegree >= 3; aveDegree--) {
-                for (
-                    let maxBitLength = 800;
-                    maxBitLength >= 200;
-                    maxBitLength -= 200
-                ) {
-                    for (let numer = 1; numer <= 5; numer++) {
-                        const denom = 20n;
-                        const round = 20;
+            for (let aveDegree = 6; aveDegree >= 3; aveDegree--) {
+                for (let numer = 2; numer <= 128; numer *= 4) {
+                    const denom = 5n;
+                    const round = 20;
 
-                        for (let i = 0; i < round; i++) {
-                            const { graph, start: v_s } = createRandomConnectedGraph(
+                    for (let i = 0; i < round; i++) {
+                        const { graph, start: v_s } =
+                            createRandomConnectedGraph(
                                 dimension,
                                 vertices,
                                 {
-                                    aveDegree: aveDegree,
-                                    maxBitLength: maxBitLength,
+                                    aveDegree,
+                                    maxBitLength: MAX_BIT_LENGTH,
                                 },
                             );
 
-                            console.log(`round ${i + 1}`)
-                            const v_t = (() => {
-                                while (true) {
-                                    const v = createVertex(
-                                        Math.floor(Math.random() * vertices),
-                                    ).expect("Never failures.")
+                        console.log(`round ${i + 1}`);
+                        const v_t = (() => {
+                            while (true) {
+                                const v = createVertex(
+                                    Math.floor(Math.random() * vertices),
+                                ).expect("Never failures.");
 
-                                    if (v !== v_s) {
-                                        return v
-                                    }
+                                if (v !== v_s) {
+                                    return v;
                                 }
-                            })()
+                            }
+                        })();
 
-                            const timeMosp = new Date();
-                            const pathMosp = solveMosp(graph, v_s, v_t, evaluator)
-                                .expect("Solve mosp error")
-                            const timeEndMosp = new Date().getTime() - timeMosp.getTime()
-                            console.log(`mosp: ${timeEndMosp}`);
+                        const timeMosp = new Date();
+                        const pathMosp = solveMosp(graph, v_s, v_t, evaluator)
+                            .expect("Solve mosp error")
+                        const timeEndMosp = new Date().getTime() - timeMosp.getTime()
+                        console.log(`mosp: ${timeEndMosp}`);
 
-                            const timeFptas = new Date();
-                            const pathFptas = solveFptasMosp(
-                                graph,
-                                v_s,
-                                v_t,
-                                evaluator,
-                                {
-                                    numer: BigInt(numer),
-                                    denom,
-                                },
-                            ).expect("Solve fptas-mosp error");
-                            const timeEndFptas = new Date().getTime() - timeFptas.getTime();
-                            console.log(`fptas_mosp: ${timeEndFptas}`);
+                        const timeFptas = new Date();
+                        const pathFptas = solveFptasMosp(
+                            graph,
+                            v_s,
+                            v_t,
+                            evaluator,
+                            {
+                                numer: BigInt(numer),
+                                denom,
+                            },
+                        ).expect("Solve fptas-mosp error");
+                        const timeEndFptas = new Date().getTime() -
+                            timeFptas.getTime();
+                        console.log(`fptas_mosp: ${timeEndFptas}`);
 
-                            const diff = evaluator(graph.getCostFromPath(pathFptas).expect(""))
-                                - evaluator(graph.getCostFromPath(pathMosp).expect(""))
+                        const diff = evaluator(graph.getCostFromPath(pathFptas).expect(""))
+                            - evaluator(graph.getCostFromPath(pathMosp).expect(""))
 
-                            Deno.writeTextFile(
-                                PATH,
-                                `${dimension}, ${vertices}, ${aveDegree}, ${maxBitLength}, ${numer / Number(denom)
-                                }, ${timeEndMosp}, ${timeEndFptas}, ${diff}\n`,
-                                { append: true },
-                            );
-                        }
+                        Deno.writeTextFile(
+                            PATH,
+                            `${dimension}, ${vertices}, ${aveDegree}, ${numer / Number(denom)
+                            }, ${timeEndMosp}, ${timeEndFptas}, ${Number(diff) / Number(MAX_COST_VALUE)}\n`,
+                            { append: true },
+                        );
                     }
                 }
             }
